bdbfa2026e9e03188d2f63198feb9c85f861e2fe
Brown-Ebouky <Brown.Ebouky@ibm.com>
2025-06-30 09:27:48 -0400

    fix: add reactivation of env in the install script

--- install_env_script.sh
+++ install_env_script.sh
@@ -5,8 +5,10 @@ set -e  # Exit on error
 # echo "Creating conda environment..."
 # conda env create -f cu124.yaml || conda env update -f cu124.yaml
 
+# Get name of current active conda environment
+CURRENT_ENV=$(basename "$CONDA_PREFIX")
+echo ">>> Installing system dependencies into current conda environment: $CURRENT_ENV"
 
-echo ">>> Installing system dependencies into current conda environment..."
 
 conda install -y -c nvidia/label/cuda-12.4.1 cuda-toolkit cuda
 conda install -y -c conda-forge gxx=13 gcc=13 libxcrypt poetry
@@ -14,7 +16,13 @@ conda install -y -c conda-forge gxx=13 gcc=13 libxcrypt poetry
 # To avoid torchinductor and modelopt errors looking for crypt.h
 conda env config vars set CPATH=$CONDA_PREFIX/include 
 
+echo ">>> Reactivating environment $CURRENT_ENV to apply env var changes..."
 echo "Activating environment..."
+
+# Load conda shell functions
+eval "$(conda shell.bash hook)"
+conda deactivate
+conda activate "$CURRENT_ENV"
 
 # other dependencies
 pip install torch==2.5.1 # +cu121
